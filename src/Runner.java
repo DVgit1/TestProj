a7b944ee9eb8abbee08b2f36a915a79cce9bf918
DVgit1 <test.dk98494dk.qa@gmail.com>
2022-05-22 11:32:21 +0200

    git pull -u origin master

---
+++ src/Runner.java
@@ -0,0 +1,18 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Runner {
+    public static void main(String[] args) {
+
+////        System.out.println("My first text");
+////        System.out.println("My second text");
+////        Scanner scanner = new Scanner(System.in);
+////        System.out.println("What`s your name?");
+////        Silnia silnia = new Silnia();
+////        int silnia1 = silnia.getSilnia(5);
+////        System.out.println(silnia1);
+//        String text = "Text";
+//        System.out.println(text.toLowerCase().startsWith("t"));
+        ex4.getTree(8);
+    }
+}
